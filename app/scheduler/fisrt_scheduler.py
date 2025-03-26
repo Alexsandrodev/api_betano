f565979dc5a7af9f298ce246ca1dbf1cc3f3b2b9
Alexsandro Mendes <alexsandroferreiramendes00@gmail.com>
2025-03-26 13:14:16 -0300

    Feat (scheduler): Adding first_scheduler to automate scraping execution

---
+++ app/scheduler/fisrt_scheduler.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler 
+from app.scraping.worker import start_scraping
+from datetime import datetime, timedelta
+
+first_scheduler = BackgroundScheduler(job_defaults={'misfire_grace_time': 60})
+first_scheduler.add_job(
+    start_scraping
+)
